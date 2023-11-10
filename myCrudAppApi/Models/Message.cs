b1c7b4310fbb279991e10bc826d0605fd12d252f
Kymani Stephens <kymanist00@gmail.com>
2023-11-10 13:21:30 -0800

    add message id

--- myCrudAppApi/Models/Message.cs
+++ myCrudAppApi/Models/Message.cs
@@ -1,7 +1,8 @@
 namespace myCrudAppApi.Models
 {
   public class Message
-  {
+  { 
+    public int MessageId { get; set; }
     public string Name { get; set; }
     public string Message { get; set; }
   }
