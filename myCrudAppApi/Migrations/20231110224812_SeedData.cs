ead9310796b0cd1fa957a22f6f8f4ef28f325898
Kymani Stephens <kymanist00@gmail.com>
2023-11-10 14:52:06 -0800

    add migrations seed data

---
+++ myCrudAppApi/Migrations/20231110224812_SeedData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myCrudAppApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "MessageId", "MessageText", "Name" },
+                values: new object[] { 1, "I'm a Kymani", "Kymani" });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "MessageId", "MessageText", "Name" },
+                values: new object[] { 2, "I'm a Qayden", "Qayden" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 2);
+        }
+    }
+}
