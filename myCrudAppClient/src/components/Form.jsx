b3d8f14a3d71b0619bb571eb31f90566346c146e
Kymani Stephens <kymanist00@gmail.com>
2023-11-11 12:29:35 -0800

    add obj to hold form data for POST request

--- myCrudAppClient/src/components/Form.jsx
+++ myCrudAppClient/src/components/Form.jsx
@@ -1,27 +1,47 @@
+import React, { useState } from "react"
+
 function Form() {
+
+    // This object represents the data from the form
+    const [formData, setFormData] = useState({
+        name: '',
+        message: '',
+    });
+
+    function handleInputChange(e) {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    }
+
 
 
     return (
         <>
-            <form action="http://www.foo.com" method="POST">
+            <form >
                 <div>
-                    <label for="say">What is your name?</label>
-                    <br></br> 
-                    <input name="name" id="name" required />
-                </div>
-
-                <br></br>
-
-                <div>
-                    <label for="message">Message:</label>
+                    <label htmlFor="name">What is your name?</label>
                     <br></br>
-                    <textarea id="message" name="message" rows="4" cols="25" required></textarea>
+                    <input name="name" id="name" value={formData.name} onChange={handleInputChange} required />
+                </div>
+
+                <br></br>
+
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <br></br>
+                    <textarea id="message" name="message" rows="4" cols="25" value={formData.message} onChange={handleInputChange} required></textarea>
                 </div>
                 <br></br>
                 <div>
-                    <button>Send my message!</button>
+                    <button>Submit</button>
                 </div>
             </form>
+
+            <p>{formData.name}</p>
+            <p>{formData.message}</p>
 
         </>
     )
